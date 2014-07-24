13c67db22eff8838fb37635ef7b439e088368171
sherry0127 <fei_fei_yang@sina.com>
2014-07-24 11:09:40 +0800

    加了注释（测试用）

--- 360FloatWindow/src/com/example/alarmclock/AlarmReceiver.java
+++ 360FloatWindow/src/com/example/alarmclock/AlarmReceiver.java
@@ -9,6 +9,8 @@ import android.os.Vibrator;
 import android.widget.Toast;
 public class AlarmReceiver extends BroadcastReceiver{
 
+	
+	
 	private Vibrator vibrator=null;
 	private MediaPlayer alarmMusic=null;
 	boolean hasVibrator=false;
@@ -16,7 +18,7 @@ public class AlarmReceiver extends BroadcastReceiver{
     public void onReceive(Context context, Intent intent) {
     	if("com.alarm.action_alarm_on".equals(intent.getAction()))
     	{
-    		Toast.makeText(context, "Alarm worked.", Toast.LENGTH_LONG).show();
+    		//Toast.makeText(context, "Alarm worked.", Toast.LENGTH_LONG).show();
     		Intent i=new Intent(context, AlarmActivity.class);
     		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
     		context.startActivity(i);
